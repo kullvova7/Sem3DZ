004dcbd2e6fcb37f41fe68e75d3bbae994975aeb
Vladimir <Vlad>
2024-02-28 09:08:19 +0300

    initial commit

---
+++ Zad2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задайте массив на 10 целых чисел. Напишите программу, 
+// которая определяет количество чётных чисел в массиве.
+// Начальные условия:
+// int[] array = {2, 5, 2, 3, 6, 3, 7, 8, 2, 4}; // Создание массива
+// Выводится: 6
+
+int[] array = { 2, 5, 2, 3, 6, 3, 7, 8, 2, 4 };
+Console.Write("Массив: ");
+    foreach (int number in array)
+        {
+            Console.Write(number + " ");
+        }
+        Console.WriteLine();
+
+        int countEvenNumbers = 0;
+    foreach (int number in array)
+        {
+            if (number % 2 == 0)
+            {
+                countEvenNumbers++;
+            }
+        }
+        Console.WriteLine("Количество чётных чисел в массиве: " + countEvenNumbers);
+
---
+++ Zad3/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задайте массив из вещественных чисел с ненулевой дробной частью.
+// Найдите разницу между максимальным и минимальным элементов массива.
+// Начальные условия:
+// int[] array = {0.25, 5.4, 1.3, 2.1, 3.8, 5.2, 3.01}; // Создание массива
+// Выводится: 5.15
+
+double[] array = { 0.25, 5.4, 1.3, 2.1, 3.8, 5.2, 3.01 };
+double min = array[0];
+double max = array[0];
+    for (int i = 1; i < array.Length; i++)
+        {
+            if (array[i] < min)
+                min = array[i];
+            if (array[i] > max)
+                max = array[i];
+        }
+double difference = max - min;
+Console.WriteLine($"Разница между макс и мин элементами: {difference}");
